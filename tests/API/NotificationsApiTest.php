70b9a0a84f91797c19657e226832cb08d4ac308a
dawid.miklas <dawid.miklas@escolasoft.com>
2023-05-22 10:23:45 +0200

    test fix

--- tests/API/NotificationsApiTest.php
+++ tests/API/NotificationsApiTest.php
@@ -237,13 +237,13 @@ class NotificationsApiTest extends TestCase
         event(new TestEvent($friend, $student, 'foo'));
         event(new DifferentTestEvent($student, 'bar'));
 
-        $response = $this->actingAs($admin, 'api')->json('GET', '/api/admin/notifications/', [
+        $response = $this->actingAs($admin, 'api')->json('GET', '/api/admin/notifications/all', [
             'event' => TestEvent::class,
         ]);
         $response->assertOk();
         $response->assertJsonCount(2, 'data');
 
-        $response = $this->actingAs($admin, 'api')->json('GET', '/api/admin/notifications/', [
+        $response = $this->actingAs($admin, 'api')->json('GET', '/api/admin/notifications/all', [
             'event' => DifferentTestEvent::class,
         ]);
         $response->assertOk();
@@ -273,20 +273,20 @@ class NotificationsApiTest extends TestCase
         $response->assertOk();
         $response->assertJsonCount(1, 'data');
 
-        $response = $this->actingAs($admin)->json('GET', '/api/admin/notifications/', [
+        $response = $this->actingAs($admin)->json('GET', '/api/admin/notifications/all', [
             'date_from' => Carbon::now()->addDay()->toIso8601String(),
         ]);
         $response->assertOk();
 
         $response->assertJsonCount(0, 'data');
 
-        $response = $this->actingAs($admin)->json('GET', '/api/admin/notifications/', [
+        $response = $this->actingAs($admin)->json('GET', '/api/admin/notifications/all', [
             'date_to' => Carbon::now()->subDay()->toIso8601String(),
         ]);
         $response->assertOk();
         $response->assertJsonCount(0, 'data');
 
-        $response = $this->actingAs($admin)->json('GET', '/api/admin/notifications/', [
+        $response = $this->actingAs($admin)->json('GET', '/api/admin/notifications/all', [
             'date_from' => Carbon::now()->subDay()->toIso8601String(),
             'date_to' => Carbon::now()->addDay()->toIso8601String(),
         ]);
@@ -294,7 +294,7 @@ class NotificationsApiTest extends TestCase
 
         $response->assertJsonCount(3, 'data');
 
-        $response = $this->actingAs($admin)->json('GET', '/api/admin/notifications/', [
+        $response = $this->actingAs($admin)->json('GET', '/api/admin/notifications/all', [
             'date_from' => Carbon::now()->subDay()->toIso8601String(),
             'date_to' => Carbon::now()->addDay()->toIso8601String(),
             'event' => TestEvent::class,
@@ -337,7 +337,7 @@ class NotificationsApiTest extends TestCase
         $this->generateEvents(null, 13);
 
         $this->actingAs($this->makeAdmin(), 'api')
-            ->getJson('/api/admin/notifications?per_page=10')
+            ->getJson('/api/admin/notifications/all?per_page=10')
             ->assertOk()
             ->assertJsonCount(10, 'data')
             ->assertJson([
@@ -348,7 +348,7 @@ class NotificationsApiTest extends TestCase
             ]);
 
         $this->actingAs($this->makeAdmin(), 'api')
-            ->getJson('/api/admin/notifications?per_page=10&page=2')
+            ->getJson('/api/admin/notifications/all?per_page=10&page=2')
             ->assertOk()
             ->assertJsonCount(3, 'data')
             ->assertJson([
